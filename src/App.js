b3c11158304ca26cec22e60366f6b2b2fa6cbb78
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-02-02 18:12:25 +0100

    Add new item

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { useState } from 'react';
 import './App.css';
 
-
 const App = () => {
-  const arrItems = ["Faire les courses", "Faire la vaisselle", "Passer l'aspirateur"]
+  const [items, setItems] = useState(["Faire les courses", "Faire la vaisselle", "Passer l'aspirateur"]);
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddButtonClick = () => {
+    const newItem = inputValue;
+  
+    const newItems = [...items, newItem];
+  
+    setItems(newItems);
+    setInputValue('');
+  };
+  
+  /* function handleChange(event) {
+    textContent = event.target.value;
+  }
+  */
 
   return (
     <div className="App">
       <h2>Hello world!</h2>
-      <input placeholder='Ajouter un item' type='text'></input>
-      <button type="submit">Ajouter</button>
+      <input value={inputValue} placeholder='Ajouter un item' type='text' onChange={(event) => setInputValue(event.target.value)}></input>
+      <button type="submit" onClick={handleAddButtonClick}>Ajouter</button>
       <ul>
-        {arrItems.map(e => {
-          return <li>{e}<button>X</button></li>
-        })}
+        {items.map(e => <li>{e}<button>X</button></li>)}
       </ul>
     </div>
   );
