d613dede698d8a4757dfdd8ec08cd135a57db576
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-02-03 06:01:13 +0100

    Add handleDelete function

--- src/App.js
+++ src/App.js
@@ -15,13 +15,20 @@ const App = () => {
     setInputValue('');
   };
   
+  const handleDelete = value => {
+    setItems(oldValues => {
+      return oldValues.filter(item => item !== value)
+    })
+  }
+
+
   return (
     <div className="App">
       <h2>To-do-list</h2>
       <input value={inputValue} placeholder='Ajouter un item' type='text' onChange={(event) => setInputValue(event.target.value)}></input>
       <button type="submit" onClick={handleAddButtonClick}>Ajouter</button>
       <ul>
-        {items.map(e => <li>{e}<button>X</button></li>)}
+        {items.map(e => <li key={e}>{e}<button onClick={() => handleDelete(e)}>X</button></li>)}
       </ul>
     </div>
   );
